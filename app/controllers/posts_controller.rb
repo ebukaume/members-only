212fd21069b8535abe7efeb38a6ebc83348d14ef
Denis Cuenca Brex <Deniscbrex.dev@gmail.com>
2019-07-23 13:25:14 -0300

    changed post and session controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,23 @@ class PostsController < ApplicationController
   before_action :logged_in?, only: [:new, :create]
   
   def new
+    @user = current_user
     @post = Post.new
-    debugger
   end
 
   def create
-
+    @user = current_user
+		if @user.posts.create(text: params[:post][:text])
+			flash.now[:success] = "Post created"
+			redirect_to root_url
+		else
+			flash.now[:danger] = "Your post can't be empty or over 140 characters :("
+			render 'new'
+		end
   end
 
   def index
+    @user = current_user
     @posts = Post.all
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,12 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:sessions][:email].downcase)
 
-    log_in @user if @user&.authenticate(params[:sessions][:password])
+    if @user&.authenticate(params[:sessions][:password])
+      log_in @user
+      flash[:success] = 'Logged'
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+  end
   end
 
   def destroy

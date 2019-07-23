d54401dcdfa1684a7ae9a3ee909fb5bfdfd7cc36
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-07-23 11:42:28 +0100

    adds Users model; session controllers; session#new form

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  has_secure_password
+  before_create :assign_remember_token
+
+  validates :password, presence: true, length: {minimum: 8}
+  validates :password_confirmation, presence: true
+
+  private
+
+  def assign_remember_token
+    token = SecureRandom.urlsafe_base64
+    remember_token = Digest::SHA1.hexdigest(token.to_s)
+    self.remember_token = remember_token
+  end
+end
